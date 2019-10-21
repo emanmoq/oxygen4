c7dec56bf0c77b07428736bc634850e9e3e4b022
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-10-21 00:00:46 -0700

    ddd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 jQuery(function ($) {
 
-	//Preloader
-	var preloader = $('.preloader');
-	$(window).load(function () {
-		preloader.remove();
-	});
+
 	$(window).scroll(function () {
         scrollTop = $(window).scrollTop();
 
@@ -22,32 +18,16 @@ jQuery(function ($) {
         });
 
 	//#main-slider
-	var  slideHeight= window.innerHeight/1.5;
+	var  slideHeight= window.innerHeight;
 
 	$('#hero-slider .item').css('height', slideHeight);
 
 
-	// navbar-fixed-top
 
 	$(window).resize(function () {
 		'use strict';
 		$('#hero-slider .item').css('height', slideHeight);
 	});
-
-
-	//Scroll Menu
-	// $(window).on('scroll', function () {
-	// 	if ($(window).scrollTop() > slideHeight) {
-	// 		$('.Main-Menu').addClass('navbar-fixed-top');
-	// 	} else {
-	// 		$('.Main-Menu').removeClass('navbar-fixed-top');
-	// 	}
-	// });
-
-	// Navigation Scroll
-	// $(window).scroll(function (event) {
-		// Scroll();
-	// });
 
 	$('.navbar-collapse ul li a').on('click', function () {
 		$('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
@@ -68,8 +48,6 @@ jQuery(function ($) {
 		new WOW().init();
 	}
 
-	//smoothScroll
-	// smoothScroll.init();
 
 	// Progress Bar
 	$('#aboutUs').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
@@ -79,52 +57,6 @@ jQuery(function ($) {
 			});
 			$(this).unbind('inview');
 		}
-	});
-
-	//Countdown
-	$('#features').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-		if (visible) {
-			$(this).find('.timer').each(function () {
-				var $this = $(this);
-				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
-					duration: 2000,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.ceil(this.Counter));
-					}
-				});
-			});
-			$(this).unbind('inview');
-		}
-	});
-
-	// Portfolio Single View
-	$('#portfolio').on('click', '.folio-read-more', function (event) {
-		event.preventDefault();
-		var link = $(this).data('single_url');
-		var full_url = '#portfolio-single-wrap',
-			parts = full_url.split("#"),
-			trgt = parts[1],
-			target_top = $("#" + trgt).offset().top;
-
-		$('html, body').animate({ scrollTop: target_top }, 600);
-		$('#portfolio-single').slideUp(500, function () {
-			$(this).load(link, function () {
-				$(this).slideDown(500);
-			});
-		});
-	});
-
-	// Close Portfolio Single View
-	$('#portfolio-single-wrap').on('click', '.close-folio-item', function (event) {
-		event.preventDefault();
-		var full_url = '#portfolio',
-			parts = full_url.split("#"),
-			trgt = parts[1],
-			target_offset = $("#" + trgt).offset(),
-			target_top = target_offset.top;
-		$('html, body').animate({ scrollTop: target_top }, 600);
-		$("#portfolio-single").slideUp(500);
 	});
 
 	// Contact form
