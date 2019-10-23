9983f80d209ecba424973b67e7129b034518bdb2
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-10-23 12:54:13 -0700

    jj

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,21 @@ jQuery(function ($) {
 		//Initiat WOW JS
 		new WOW().init();
 	}
-
+	$('#features').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.counter').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
 
 
 	// Contact form
